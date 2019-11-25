6bdc8dfcd9b3a72ea0f0a7600bd35fa3c4861959
ahmaddregat <ahmad.dregat.97@hotmail.com>
2019-11-25 16:25:59 +0200

    fixing the logo & fixing the buttons

--- app/src/main/java/com/example/mymedicine/loginActivity.java
+++ app/src/main/java/com/example/mymedicine/loginActivity.java
@@ -1,17 +1,43 @@
 package com.example.mymedicine;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class loginActivity extends AppCompatActivity {
-private EditText reg ;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        TextView textView = findViewById(R.id.textView);
+
+        String text = "Not registered ? sign up Here.";
+
+        SpannableString ss = new SpannableString(text);
+
+        ClickableSpan clickableSpan1 = new ClickableSpan() {
+            @Override
+            public void onClick(View widget) {
+                movetologinactivity();
+            }
+        };
+
+        ss.setSpan(clickableSpan1, 17,30, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        textView.setText(ss);
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
+    }
+    public void movetologinactivity () {
+        Intent intent = new Intent(loginActivity.this, registerActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/mymedicine/registerActivity.java
+++ app/src/main/java/com/example/mymedicine/registerActivity.java
@@ -2,13 +2,40 @@ package com.example.mymedicine;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.view.View;
+import android.widget.TextView;
 
 public class registerActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        TextView textView = findViewById(R.id.textView3);
+
+        String text = "Already have an account ? Sign in here ";
+
+        SpannableString ss = new SpannableString(text);
+
+        ClickableSpan clickableSpan1 = new ClickableSpan() {
+            @Override
+            public void onClick(View widget) {
+                moveifuhaveaccuont();
+            }
+        };
+
+        ss.setSpan(clickableSpan1, 26,38, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);` `
+        textView.setText(ss);
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
+    }
+
+    public void moveifuhaveaccuont () {
+        Intent intent = new Intent( registerActivity.this,loginActivity.class);
+        startActivity(intent);
     }
 }
